a3d933109e6d92165783f2333f34fd6a40cd2012
Arina Nov <arinanovikova710@gmail.com>
2026-06-26 00:15:39 +0600

    Выполнение задания 1, тесты и CI

---
+++ task01/task01/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return false;
+
+        ReadOnlySpan<char> span = input.AsSpan();
+        Span<char> cleaned = stackalloc char[span.Length];
+        int cleanedLength = 0;
+
+        foreach (char c in span)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                cleaned[cleanedLength++] = char.ToLowerInvariant(c);
+            }
+        }
+
+        if (cleanedLength == 0) return false;
+
+        int left = 0;
+        int right = cleanedLength - 1;
+
+        while (left < right)
+        {
+            if (cleaned[left] != cleaned[right]) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
