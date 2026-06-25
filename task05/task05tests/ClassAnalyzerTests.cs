815445d1303446f1fd9858d49f8d37550814289d
Arina Nov <arinanovikova710@gmail.com>
2026-06-26 01:02:59 +0600

    Выполнение задания 5, рефлексия и тесты

---
+++ task05/task05tests/ClassAnalyzerTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using task05;
+
+namespace task05tests;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField = string.Empty;
+    public int Property { get; set; }
+    public void Method() { }
+    public string ComplexMethod(int id, string value) => value;
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+        Assert.Contains("Method", methods);
+        Assert.Contains("ComplexMethod", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+        Assert.Contains("Property", properties);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsTrue_WhenAttributeExists()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsFalse_WhenAttributeMissing()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        Assert.False(analyzer.HasAttribute<SerializableAttribute>());
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnsCorrectParameters()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var parameters = analyzer.GetMethodParams("ComplexMethod").ToList();
+        Assert.Equal(2, parameters.Count);
+        Assert.Contains("Int32 id", parameters);
+        Assert.Contains("String value", parameters);
+    }
+}
+
